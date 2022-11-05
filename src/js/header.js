1a2fcc7729d0fd5613d6a04267b785a6fe5fe6a3
proflinervk <proflinervk@gmail.com>
2022-11-06 01:14:36 +0300

    create price-on with adaptive

--- src/js/header.js
+++ src/js/header.js
@@ -10,7 +10,6 @@ document.addEventListener( 'DOMContentLoaded', () => {
 window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY
     const header = document.querySelector( '.header' )
-    const rate = document.querySelector( '.rate-link' )
 
     if (scrollTop > 0) {
         if ( ! header.classList.contains( 'scrolled' ) )
@@ -19,13 +18,6 @@ window.addEventListener('scroll', () => {
     }   else {
         if ( header.classList.contains( 'scrolled' ) )
             header.classList.remove( 'scrolled' )
-    }
-
-    if( scrollTop > 1000 ) {
-        rate.classList.add( 'scrolled' )
-    } else {
-        if( scrollTop < 400 )
-        rate.classList.remove( 'scrolled' )
     }
 })
 
