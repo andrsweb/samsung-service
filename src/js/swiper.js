17016b82eb33b55de754f53a2e02f82037950779
proflinervk <proflinervk@gmail.com>
2022-11-06 19:58:28 +0300

    create testimonials desktop

---
+++ src/js/swiper.js
@@ -0,0 +1,18 @@
+import Swiper, { Scrollbar } from 'swiper';
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	'use strict'
+
+	testiSwiper()
+} )
+
+const testiSwiper = () => {
+
+	const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        direction: 'horizontal',
+		loop: true,
+        spaceBetween: 50,
+        grabCursor: 1,
+	} )
+}
