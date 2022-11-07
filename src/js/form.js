0fbde9c3f0a77f0f512f149ab759c36cc86b2fe9
proflinervk <proflinervk@gmail.com>
2022-11-08 02:39:09 +0300

    add phone link

--- src/js/form.js
+++ src/js/form.js
@@ -1,37 +1,79 @@
-import { disableBodyScroll, enableBodyScroll, } from 'body-scroll-lock'
-import { setTargetElement, getTargetElement } from './common/global'
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 
 document.addEventListener( 'DOMContentLoaded', () => {
-    'use strict'
+	'use strict'
 
-    switchForm()
-})
+	formOnClick()
+	submitForm()
+} )
 
-const switchForm = () => {
-    const headerForm        =   document.querySelector( '.header-form' )
-    const formButtons       =   document.querySelectorAll( '.button.open-form' )
+const formOnClick = () => {
+	const formWrapper = document.querySelector( '.form-wrapper' )
+	const formButton  = document.querySelectorAll( '.header-button' )
+	const closeButton = document.querySelector( '.close-button' )
+	const targetElement  = document.querySelector( '#body-lock' )
 
-    if ( ( ! headerForm ) && ( ! formButtons ) ) return
+	if ( ! formWrapper && ! formButton ) return
 
-    formButtons.forEach( formButton => {
+	formButton.forEach( button => {
+		button.addEventListener( 'click', () => {
+			if ( ! formWrapper.classList.contains( 'openned' ) ) {
+				disableBodyScroll( targetElement )
+				formWrapper.classList.add( 'opened' )
+			} else {
+				formWrapper.classList.remove( 'opened' )
+				enableBodyScroll( targetElement )
+			}
+		} )
+	} )
 
-        formButton.addEventListener( 'click', () => {
-            setTargetElement( document.querySelector( '#form-lock' ) )
-            if ( ! headerForm.classList.contains( 'opened' ) ) {
-                headerForm.classList.add( 'opened' )
-                disableBodyScroll( getTargetElement() )
-            }
-        } )
-    } )
+	closeButton.addEventListener( 'click', () => { //close form by click cross
+		formWrapper.classList.remove( 'opened' )
+		enableBodyScroll( targetElement )
+	} )
 
-    headerForm.addEventListener( 'click', e => {
+	formWrapper.addEventListener( 'click', e => {  //close for for touch anywhere
         e.stopPropagation()
 
         const target = e.target
 
-        if ( target.className && target.classList.contains( 'header-form' ) ) {
-            enableBodyScroll( getTargetElement() )
-            headerForm.classList.remove( 'opened' )
-        }
+        if ( target.className && target.classList.contains( 'form-wrapper' ) ) {
+			formWrapper.classList.remove( 'opened' )
+			enableBodyScroll( targetElement )
+		}
     } )
+}
+
+const submitForm = () => {
+	const forms			= document.querySelectorAll( '.form' )
+
+	if( ! forms.length ) return
+
+	forms.forEach( form => {
+		form.addEventListener( 'submit', e => {
+			e.preventDefault()
+
+			const formResponse	= form.querySelector( '.form-response' ),
+				request		= new XMLHttpRequest(),
+				formData		= new FormData( form )
+
+			request.open( 'post', 'send-form.php', true )
+
+			formResponse.classList.remove( ['success', 'error'] )
+			formResponse.textContent = 'Обработка...'
+
+			request.addEventListener( 'load', () => {
+				if  ( request.status === 200 ) {
+					formResponse.classList.add( 'success' )
+				} else {
+					formResponse.classList.add( 'error' )
+					console.error( request.response )
+				}
+
+				formResponse.textContent = request.response
+			} )
+
+			request.send( formData )
+		} )
+	} )
 }
